afb62ed5924aa735e7602ac2b93c980493eca2b4
Tu Houwang <tuhouwang96@163.com>
2021-01-25 15:08:04 +0800

    修改了筛选模态的条件

--- src/NumOfModes.m
+++ src/NumOfModes.m
@@ -2,20 +2,20 @@ function [nmodes, kr, v1, v2] = NumOfModes(w, kr, v1, v2, cpmax)
 
     cp     = w ./ real(kr);
 
-    nmodes = length( find( cp <= cpmax ) );
+    mode = find( cp <= cpmax );
+    kr = kr(   mode);
+    v1 = v1(:, mode);
+    v2 = v2(:, mode);
+    
+    mode = find( imag(kr) >= 0 & imag(kr) < real(kr) );
+    kr = kr(   mode);
+    v1 = v1(:, mode);
+    v2 = v2(:, mode);
+    
+    nmodes = length( kr );
 
     if(nmodes == 0)
         error('Incorrect maximum phase speed input!');
     end
-
-    kr = kr(   1 : nmodes);
-    v1 = v1(:, 1 : nmodes);
-    v2 = v2(:, 1 : nmodes);
-    
-%     mode = find( imag(kr) >= 0 ) ;
-%     nmodes = length( find( imag(kr) >= 0 ) );
-%     kr = kr(   mode);
-%     v1 = v1(:, mode);
-%     v2 = v2(:, mode);
     
 end
